b2e374d66fb35e2f4d0db5913e80224d3a1df7a6
Michael Dominick <michael@Michaels-MacBook-Pro.local>
2015-05-27 13:30:40 -0400

    Added CreditCardProcessorHelper class complete with sample code

---
+++ PaymentTransactions/CreditCardProcessor.java
@@ -0,0 +1,51 @@
+import java.math.BigDecimal;
+
+/**
+ * Created by michael on 5/27/15.
+ */
+public class CreditCardProcessor {
+
+/* Fields */
+
+    private String appId;
+    private String transKey;
+
+/* Singleton */
+
+    private static CreditCardProcessor _instance;
+    public static CreditCardProcessor instance() {
+        if (_instance == null) _instance = new CreditCardProcessor();
+        return _instance;
+    }
+
+    public void config(String appLoginId, String transactionKey) {
+        CreditCardProcessor.instance().appId = appLoginId;
+        CreditCardProcessor.instance().transKey = transactionKey;
+    }
+
+    public void runCreditCardTransaction(String cardNumber, String expMonth, String expYear, BigDecimal amt) {
+        CreditCard card = new CreditCard();
+        card.setCreditCardNumber(cardNumber);
+        card.setExpirationMonth(expMonth);
+        card.setExpirationYear(expYear);
+
+        Transaction trans = merchant.createAIMTransaction(
+                TransactionType.AUTH_CAPTURE, amt);
+        trans.setCreditCard(card);
+
+        Result<Transaction> result = (Result<Transaction>)merchant
+                .postTransaction(trans);
+
+        if(result.isApproved()) {
+            System.out.println("Approved!");
+            System.out.println(" Id: " + result.getTarget().getTransactionId());
+        } else if (result.isDeclined()) {
+            System.out.println("Declined by issuing bank");
+            System.out.println(result.getReasonResponseCode() + " : " + result.getResponseText());
+        } else {
+            System.out.println("Unable to process transaction");
+            Sytem.out.println(result.getReasonResponseCode() + " : " + result.getResponseText());
+        }
+    }
+
+}
