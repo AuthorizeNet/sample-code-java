0438fdf85cdbabcf5f0899ae509238cfd41c9adc
brianmc <bmcmanus@authorize.net>
2015-05-31 23:18:41 -0700

    moved to a simple standard java code hierarchy

---
+++ src/main/java/net/authorize/sdk/VisaCheckout/VisaCheckoutDecrypt.java
@@ -0,0 +1,9 @@
+package net.authorize.sdk.samplecode.visacheckout;
+
+public class VisaCheckoutDecrypt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
