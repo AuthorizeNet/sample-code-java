6847a75fb88d7dfe684a4382f8407d344439d604
brianmc <bmcmanus@authorize.net>
2015-06-02 07:05:35 -0700

    updated

--- src/main/java/net/authorize/sdk/VisaCheckout/VisaCheckoutDecrypt.java
+++
@@ -1,9 +0,0 @@
-package net.authorize.sdk.samplecode.visacheckout;
-
-public class VisaCheckoutDecrypt {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-
-}
