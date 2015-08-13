55c8851198bb5992f45abb03c6229eaa7d0f8edf
avaradha <avaradha@visa.com>
2015-08-13 12:09:51 -0700

    Added samples

---
+++ src/main/java/net/authorize/sample/ManageCustomerProfiles/CreateCustomerPaymentProfile.java
@@ -0,0 +1,82 @@
+package net.authorize.sample.TransactionDetails;
+
+import net.authorize.Environment;
+import net.authorize.api.contract.v1.*;
+
+import net.authorize.api.contract.v1.MerchantAuthenticationType;
+//import net.authorize.api.contract.v1.TransactionDetailsType;
+//import net.authorize.api.controller.GetTransactionDetailsController;
+import net.authorize.api.controller.base.ApiOperationBase;
+
+import net.authorize.Transaction;
+import net.authorize.api.contract.v1.CreateCustomerPaymentProfileRequest;
+import net.authorize.api.contract.v1.CreateCustomerPaymentProfileResponse;
+import net.authorize.api.contract.v1.CreateCustomerProfileFromTransactionRequest;
+import net.authorize.api.contract.v1.CreateCustomerProfileRequest;
+import net.authorize.api.contract.v1.CreateCustomerProfileResponse;
+import net.authorize.api.contract.v1.CreditCardType;
+import net.authorize.api.contract.v1.CustomerAddressType;
+import net.authorize.api.contract.v1.CustomerPaymentProfileType;
+import net.authorize.api.contract.v1.CustomerProfileType;
+import net.authorize.api.contract.v1.GetCustomerPaymentProfileRequest;
+import net.authorize.api.contract.v1.GetCustomerPaymentProfileResponse;
+import net.authorize.api.contract.v1.GetTransactionDetailsRequest;
+import net.authorize.api.contract.v1.GetTransactionDetailsResponse;
+import net.authorize.api.contract.v1.MerchantAuthenticationType;
+import net.authorize.api.contract.v1.MessageTypeEnum;
+import net.authorize.api.contract.v1.TransactionDetailsType;
+import net.authorize.api.contract.v1.ValidationModeEnum;
+import net.authorize.api.controller.CreateCustomerProfileController;
+import net.authorize.api.controller.CreateCustomerProfileFromTransactionController;
+import net.authorize.api.controller.CreateCustomerPaymentProfileController;
+import net.authorize.api.controller.base.ApiOperationBase;
+import net.authorize.cim.Result;
+import net.authorize.cim.TransactionType;
+import net.authorize.cim.ValidationModeType;
+
+//author @krgupta
+public class CreateCustomerPaymentProfile {
+	
+	public static void run(String apiLoginId, String transactionKey) {
+
+        ApiOperationBase.setEnvironment(Environment.SANDBOX);
+
+        MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
+        merchantAuthenticationType.setName("5KP3u95bQpv");
+        merchantAuthenticationType.setTransactionKey("4Ktq966gC55GAX7S");
+        ApiOperationBase.setMerchantAuthentication(merchantAuthenticationType);
+		
+		String customerprofileId = "36374423" ;
+		
+		
+	//private String getPaymentDetails(MerchantAuthenticationType merchantAuthentication, String customerprofileId, ValidationModeEnum validationMode) {
+		CreateCustomerPaymentProfileRequest getRequest = new CreateCustomerPaymentProfileRequest();
+		getRequest.setMerchantAuthentication(merchantAuthenticationType);
+		getRequest.setCustomerProfileId(customerprofileId);	
+
+		//customer address
+		CustomerAddressType customerAddress = new CustomerAddressType();
+		customerAddress.setFirstName("test");
+		customerAddress.setLastName("scenario");
+		customerAddress.setAddress("123 Main Street");
+		customerAddress.setCity("Bellevue");
+		customerAddress.setState("WA");
+		customerAddress.setZip("98004");
+		customerAddress.setCountry("USA");
+		customerAddress.setPhoneNumber("000-000-0000");
+		
+		//credit card details
+		CreditCardType creditCard = new CreditCardType();
+		creditCard.setCardNumber("4111111111111111");
+		creditCard.setExpirationDate("2023-12");
+		creditCard.setCardCode("");
+	
+        CreateCustomerPaymentProfileController controller = new CreateCustomerPaymentProfileController(getRequest);
+        controller.execute();
+       
+		CreateCustomerPaymentProfileResponse getResponse = new CreateCustomerPaymentProfileResponse();
+		getResponse = controller.getApiResponse();
+
+		System.out.println ("customerPaymentProfileId :" + getResponse.getCustomerPaymentProfileId());
+		}	
+	}
--- src/main/java/net/authorize/sample/RecurringBilling/CreateSubscription.java
+++ src/main/java/net/authorize/sample/RecurringBilling/CreateSubscription.java
@@ -1,1 +1,91 @@
+/*import net.authorize.data.arb.*;
+import java.math.BigDecimal;
 
+import net.authorize.Environment;
+import net.authorize.data.Customer;
+import net.authorize.api.contract.v1.*;
+import net.authorize.api.controller.base.ApiOperationBase;
+import net.authorize.api.controller.CreateTransactionController;
+
+
+public class CreateSubscription {
+
+    //
+    // Run this sample from command line with:
+    //                 java -jar target/ChargeCreditCard-jar-with-dependencies.jar
+    //
+    public static void run(String apiLoginId, String transactionKey) {
+
+
+        //Common code to set for all requests
+        ApiOperationBase.setEnvironment(Environment.SANDBOX);
+
+        MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
+        merchantAuthenticationType.setName("5KP3u95bQpv");
+        merchantAuthenticationType.setTransactionKey("4Ktq966gC55GAX7S");
+        ApiOperationBase.setMerchantAuthentication(merchantAuthenticationType);
+
+        // Set up payment schedule
+        PaymentSchedule schedule = PaymentSchedule.createPaymentSchedule();
+        schedule.setIntervalLength(1);
+        schedule.setSubscriptionUnit(SubscriptionUnitType.MONTHS);
+        schedule.setStartDate("2020-08-30"); //2020-08-30
+        schedule.setTotalOccurrences(12);
+        schedule.setTrialOccurrences(1);
+
+        // Set up customer information
+        Customer customer = Customer.createCustomer();
+        customer.setFirstName("John");
+        customer.setLastName("Smith");
+
+        // Populate subscription
+        Subscription subscription = Subscription.createSubscription();
+        subscription.setSchedule(schedule);
+        subscription.setAmount(new BigDecimal(10.29));
+        subscription.setTrialAmount(new BigDecimal(0.00));
+        subscription.setCustomer(customer);
+
+        // Populate the payment data
+        PaymentType paymentType = new PaymentType();
+        CreditCardType creditCard = new CreditCardType();
+        creditCard.setCardNumber("4111111111111111");
+        creditCard.setExpirationDate("1220");
+        paymentType.setCreditCard(creditCard);
+
+        ARBSubscriptionType arbSubscriptionType = new ARBSubscriptionType();
+
+        // Make the API Request
+        ARBCreateSubscriptionRequest apiRequest = new ARBCreateSubscriptionRequest();
+        /*apiRequest.setTransactionRequest(txnRequest);
+        CreateTransactionController controller = new CreateTransactionController(apiRequest);
+        controller.execute();
+
+
+        CreateTransactionResponse response = controller.getApiResponse();
+
+        if (response!=null) {
+
+            // If API Response is ok, go ahead and check the transaction response
+            if (response.getMessages().getResultCode() == MessageTypeEnum.OK) {
+
+                TransactionResponse result = response.getTransactionResponse();
+                if (result.getResponseCode().equals("1")) {
+                    System.out.println(result.getResponseCode());
+                    System.out.println("Successful Credit Card Transaction");
+                    System.out.println(result.getAuthCode());
+                    System.out.println(result.getTransId());
+                }
+                else
+                {
+                    System.out.println("Failed Transaction"+result.getResponseCode());
+                }
+            }
+            else
+            {
+                System.out.println("Failed Transaction:  "+response.getMessages().getResultCode());
+            }
+        }
+    }
+
+
+}*/
