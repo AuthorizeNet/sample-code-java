5bf1a1c9237a6d3bdb7e28470dc0caeb5f8356da
brianmc <bmcmanus@authorize.net>
2015-08-14 10:16:05 -0700

    Added samples for Customer Profiles

--- src/main/java/net/authorize/sample/CustomerProfiles/ValidateCustomerPaymentProfile.java
+++ src/main/java/net/authorize/sample/CustomerProfiles/ValidateCustomerPaymentProfile.java
@@ -1,6 +1,6 @@
 package CustomerProfiles;
 
-public class CreateCustomerProfile {
+public class ValidateCustomerPaymentProfile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
