eae640b991b18919d944002e053c291c6b7b3318
krgupta <krgupta@visa.com>
2015-09-03 16:17:53 -0700

    Transaction Reporting Test

--- src/main/java/net/authorize/sample/TransactionReporting/GetBatchStatistics.java
+++ src/main/java/net/authorize/sample/TransactionReporting/GetBatchStatistics.java
@@ -1,10 +1,41 @@
-package TransactionReporting;
+package net.authorize.sample.TransactionReporting;
 
+import net.authorize.Environment;
+import net.authorize.api.contract.v1.*;
+import net.authorize.api.controller.GetBatchStatisticsController;
+import net.authorize.api.controller.base.ApiOperationBase;
+
+//author @krgupta
 public class GetBatchStatistics {
+	
+		public static void run(String apiLoginId, String transactionKey) {
+			ApiOperationBase.setEnvironment(Environment.SANDBOX);
 
-	public static void main(String apiLoginId, String transactionKey) {
-		// TODO Auto-generated method stub
-		System.out.println("Get Batch Statistics Sample");
-	}
+	        MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
+	        merchantAuthenticationType.setName(apiLoginId);
+	        merchantAuthenticationType.setTransactionKey(transactionKey);
+	        ApiOperationBase.setMerchantAuthentication(merchantAuthenticationType);
 
+			GetBatchStatisticsRequest getRequest = new GetBatchStatisticsRequest();
+            getRequest.setMerchantAuthentication(merchantAuthenticationType);
+            String batchId = "12345";
+            getRequest.setBatchId(batchId);
+
+            GetBatchStatisticsController controller = new GetBatchStatisticsController(getRequest);
+            controller.execute();
+            GetBatchStatisticsResponse getResponse = controller.getApiResponse();
+			
+			if (getResponse!=null) {
+
+            	 if (getResponse.getMessages().getResultCode() == MessageTypeEnum.OK) {
+
+                	System.out.println(getResponse.getMessages().getMessage().get(0).getCode());
+                	System.out.println(getResponse.getMessages().getMessage().get(0).getText());
+            	}
+            	else
+            	{
+                	System.out.println("Failed to cancel Subscription:  " + getResponse.getMessages().getResultCode());
+            	}
+       	 	}
+    	}
 }
