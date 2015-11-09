01021596d4625ed2b3b54faa468841aa5c602027
kaumahat <kaumahat@visa.com>
2015-11-09 15:23:18 +0530

    Fixed GetSettledBatchList Sample Code

--- src/main/java/net/authorize/sample/SampleCode.java
+++ src/main/java/net/authorize/sample/SampleCode.java
@@ -92,7 +92,7 @@ public class SampleCode {
         System.out.println("    UpdateSubscription");
         System.out.println("    GetListOfSubscriptions");
         System.out.println("    GetBatchStatistics");
-        //System.out.println("    GetSettledBatchList");
+        System.out.println("    GetSettledBatchList");
         System.out.println("    GetTransactionList");
         System.out.println("    GetUnsettledTransactionList");
         System.out.println("    GetTransactionDetails");
@@ -188,9 +188,9 @@ public class SampleCode {
             case "GetBatchStatistics":
                 GetBatchStatistics.run(apiLoginId, transactionKey);
                 break;
-            /*case "GetSettledBatchList":
+            case "GetSettledBatchList":
                 GetSettledBatchList.run(apiLoginId, transactionKey);
-                break;*/
+                break;
             case "GetTransactionList":
                 GetTransactionList.run(apiLoginId, transactionKey);
                 break;
--- src/main/java/net/authorize/sample/TransactionReporting/GetSettledBatchList.java
+++ src/main/java/net/authorize/sample/TransactionReporting/GetSettledBatchList.java
@@ -1,11 +1,13 @@
 package net.authorize.sample.TransactionReporting;
 
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
+import java.util.GregorianCalendar;
 import net.authorize.Environment;
 import net.authorize.api.contract.v1.*;
 import net.authorize.api.controller.GetSettledBatchListController;
 import net.authorize.api.controller.base.ApiOperationBase;
 
-//author @krgupta
+//author @krgupta modified @kikmak42
 public class GetSettledBatchList {
 
      public static void run(String apiLoginId, String transactionKey) {
@@ -19,15 +21,30 @@ public class GetSettledBatchList {
             
         GetSettledBatchListRequest getRequest = new GetSettledBatchListRequest();
         getRequest.setMerchantAuthentication(merchantAuthenticationType);
+        
+        // Set first settlement date in format (year, month, day)
+        getRequest.setFirstSettlementDate(new XMLGregorianCalendarImpl(new GregorianCalendar(2015, 10, 8)));
+        
+        // Set last settlement date in format (year, month, day) (should not be greater that 31 days since first settlement date)
+        getRequest.setLastSettlementDate(new XMLGregorianCalendarImpl(new GregorianCalendar(2015, 11, 8)));
+        
         GetSettledBatchListController controller = new GetSettledBatchListController(getRequest);
         controller.execute();
-        GetSettledBatchListResponse getResponse = new GetSettledBatchListResponse();
+        GetSettledBatchListResponse getResponse = controller.getApiResponse();
          if (getResponse!=null) {
 
              if (getResponse.getMessages().getResultCode() == MessageTypeEnum.OK) {
 
                 System.out.println(getResponse.getMessages().getMessage().get(0).getCode());
                 System.out.println(getResponse.getMessages().getMessage().get(0).getText());
+                
+                ArrayOfBatchDetailsType batchList = getResponse.getBatchList();
+                if(batchList != null){
+                    System.out.println("List of Settled Transaction :");
+                    for (BatchDetailsType batch : batchList.getBatch()) {
+                        System.out.println(batch.getBatchId() + " - " + batch.getMarketType() + " - " + batch.getPaymentMethod() + " - " + batch.getProduct() + " - " + batch.getSettlementState());
+                    }
+                 }
             }
             else
             {
