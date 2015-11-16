8db3cd1cee36f8198dae31eb2311e01a06c19285
Kaushik <kikmak42@null.net>
2015-11-16 17:32:23 +0530

    XMLGeorgianCalendar object created using DataFactory -> GetSettledBatchList

--- src/main/java/net/authorize/sample/TransactionReporting/GetSettledBatchList.java
+++ src/main/java/net/authorize/sample/TransactionReporting/GetSettledBatchList.java
@@ -1,12 +1,12 @@
 package net.authorize.sample.TransactionReporting;
 
-import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 import java.time.ZonedDateTime;
 import java.util.GregorianCalendar;
 import net.authorize.Environment;
 import net.authorize.api.contract.v1.*;
 import net.authorize.api.controller.GetSettledBatchListController;
 import net.authorize.api.controller.base.ApiOperationBase;
+import javax.xml.datatype.DatatypeFactory;
 
 //author @krgupta modified @kikmak42
 public class GetSettledBatchList {
@@ -23,13 +23,19 @@ public class GetSettledBatchList {
         GetSettledBatchListRequest getRequest = new GetSettledBatchListRequest();
         getRequest.setMerchantAuthentication(merchantAuthenticationType);
         
-        // Set first settlement date in format (year, month, day)(should not be less that 31 days since last settlement date)
-        GregorianCalendar pastDate = GregorianCalendar.from(ZonedDateTime.now().minusDays(30));
-        getRequest.setFirstSettlementDate(new XMLGregorianCalendarImpl(pastDate));
-        
-        // Set last settlement date in format (year, month, day) (should not be greater that 31 days since first settlement date)
-        GregorianCalendar currentDate = GregorianCalendar.from(ZonedDateTime.now());
-        getRequest.setLastSettlementDate(new XMLGregorianCalendarImpl(currentDate));
+        try {
+            // Set first settlement date in format (year, month, day)(should not be less that 31 days since last settlement date)
+            GregorianCalendar pastDate = GregorianCalendar.from(ZonedDateTime.now().minusDays(30));
+            getRequest.setFirstSettlementDate(DatatypeFactory.newInstance().newXMLGregorianCalendar(pastDate));
+
+            // Set last settlement date in format (year, month, day) (should not be greater that 31 days since first settlement date)
+            GregorianCalendar currentDate = GregorianCalendar.from(ZonedDateTime.now());
+            getRequest.setLastSettlementDate(DatatypeFactory.newInstance().newXMLGregorianCalendar(currentDate));
+
+        } catch (Exception ex) {
+            System.out.println("Error : while setting dates");
+            ex.printStackTrace();
+        }
         
         GetSettledBatchListController controller = new GetSettledBatchListController(getRequest);
         controller.execute();
