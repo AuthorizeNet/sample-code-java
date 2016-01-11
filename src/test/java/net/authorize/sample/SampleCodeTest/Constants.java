73f9231b437a3ee49ea88cd839a50d7f902bcfa0
srathod <srathod@visa.com>
2016-01-11 22:21:28 +0530

    - Moved SampleCodeTest package to src/test/java.

--- src/test/java/net/authorize/sample/SampleCodeTest/Constants.java
+++ src/test/java/net/authorize/sample/SampleCodeTest/Constants.java
@@ -5,5 +5,5 @@ public final class Constants {
 	public static final String TRANSACTION_KEY = "4Ktq966gC55GAX7S";
 	public static final String TRANSACTION_ID = "123456";
 	public static final String PAYER_ID = "6ZSCSYG33VP8Q";
-	public static final String CONFIG_FILE = "./src/main/java/net/authorize/sample/SampleCodeTest/SampleCodeList.txt";
+	public static final String CONFIG_FILE = "./src/test/java/net/authorize/sample/SampleCodeTest/SampleCodeList.txt";
 }
