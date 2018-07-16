ef5df32d772f1534075413a5efe035239698f1f3
Kaushik Kumar Mahato <kikmak42@null.net>
2018-07-16 15:31:48 +0530

    Update CreditBankAccount.java

--- src/main/java/net/authorize/sample/PaymentTransactions/CreditBankAccount.java
+++ src/main/java/net/authorize/sample/PaymentTransactions/CreditBankAccount.java
@@ -39,7 +39,7 @@ public class CreditBankAccount {
         PaymentType paymentType = new PaymentType();
         BankAccountType bankAccountType = new BankAccountType();
         bankAccountType.setAccountType(BankAccountTypeEnum.CHECKING);
-        bankAccountType.setRoutingNumber("125008547");
+        bankAccountType.setRoutingNumber("121141754");
         bankAccountType.setAccountNumber("1234567890");
         bankAccountType.setNameOnAccount("John Doe");
         paymentType.setBankAccount(bankAccountType);
