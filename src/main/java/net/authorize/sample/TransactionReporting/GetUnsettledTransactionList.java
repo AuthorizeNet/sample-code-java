b0170d6653d8c184aa1017caf200affeedb3c990
Ashutosh Singh <ashtru@users.noreply.github.com>
2018-08-13 22:30:08 +0530

    Corrected formatting
    Corrected formatting

--- src/main/java/net/authorize/sample/TransactionReporting/GetUnsettledTransactionList.java
+++ src/main/java/net/authorize/sample/TransactionReporting/GetUnsettledTransactionList.java
@@ -9,48 +9,45 @@ import net.authorize.api.controller.base.ApiOperationBase;
 
 //author @krgupta
 public class GetUnsettledTransactionList{
-	
-		public static ANetApiResponse run(String apiLoginId, String transactionKey) {
-			ApiOperationBase.setEnvironment(Environment.SANDBOX);
+    public static ANetApiResponse run(String apiLoginId, String transactionKey) {
+        ApiOperationBase.setEnvironment(Environment.SANDBOX);
 
-	        MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
-	        merchantAuthenticationType.setName(apiLoginId);
-	        merchantAuthenticationType.setTransactionKey(transactionKey);
-	        ApiOperationBase.setMerchantAuthentication(merchantAuthenticationType);
+        MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
+        merchantAuthenticationType.setName(apiLoginId);
+        merchantAuthenticationType.setTransactionKey(transactionKey);
+        ApiOperationBase.setMerchantAuthentication(merchantAuthenticationType);
 	
-			GetUnsettledTransactionListRequest getRequest = new GetUnsettledTransactionListRequest();
-			getRequest.setMerchantAuthentication(merchantAuthenticationType);
-			getRequest.setStatus(TransactionGroupStatusEnum.ANY);
+        GetUnsettledTransactionListRequest getRequest = new GetUnsettledTransactionListRequest();
+        getRequest.setMerchantAuthentication(merchantAuthenticationType);
+        getRequest.setStatus(TransactionGroupStatusEnum.ANY);
 			
-	        Paging paging = new Paging();
-	        paging.setLimit(100);
-	        paging.setOffset(1);
+        Paging paging = new Paging();
+        paging.setLimit(100);
+        paging.setOffset(1);
 	        
-			getRequest.setPaging(paging);
+        getRequest.setPaging(paging);
 			
-			TransactionListSorting sorting = new TransactionListSorting();
-			sorting.setOrderBy(TransactionListOrderFieldEnum.ID);
-			sorting.setOrderDescending(true);
+        TransactionListSorting sorting = new TransactionListSorting();
+        sorting.setOrderBy(TransactionListOrderFieldEnum.ID);
+        sorting.setOrderDescending(true);
 			
-			getRequest.setSorting(sorting);
+        getRequest.setSorting(sorting);
 
-  			GetUnsettledTransactionListController controller = new GetUnsettledTransactionListController(getRequest);
-            controller.execute();
-            GetUnsettledTransactionListResponse getResponse = controller.getApiResponse();
+        GetUnsettledTransactionListController controller = new GetUnsettledTransactionListController(getRequest);
+        controller.execute();
+        GetUnsettledTransactionListResponse getResponse = controller.getApiResponse();
 
-            if (getResponse!=null) {
+        if (getResponse!=null) {
+            if (getResponse.getMessages().getResultCode() == MessageTypeEnum.OK) {
+                System.out.println(getResponse.getMessages().getMessage().get(0).getCode());
+                System.out.println(getResponse.getMessages().getMessage().get(0).getText());
+            }
+            else
+            {
+                System.out.println("Failed to get unsettled transaction list:  " + getResponse.getMessages().getResultCode());
+            }
+        }
+        return getResponse;
 
-            	 if (getResponse.getMessages().getResultCode() == MessageTypeEnum.OK) {
-
-                	System.out.println(getResponse.getMessages().getMessage().get(0).getCode());
-                	System.out.println(getResponse.getMessages().getMessage().get(0).getText());
-            	}
-            	else
-            	{
-                	System.out.println("Failed to get unsettled transaction list:  " + getResponse.getMessages().getResultCode());
-            	}
-       	 	}
-			return getResponse;
-
-	}
+    }
 }
