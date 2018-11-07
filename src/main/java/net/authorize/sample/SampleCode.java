ba06a5cd1e1b5804cf5ef75041642e06a630a118
JeevapriyaC <30900716+JeevapriyaC@users.noreply.github.com>
2018-11-07 17:09:57 +0530

    GetAccountUpdateJobDetails review comment chnges(Default credentials added)
    GetAccountUpdateJobDetails review comment chnges(Default credentials added)

--- src/main/java/net/authorize/sample/SampleCode.java
+++ src/main/java/net/authorize/sample/SampleCode.java
@@ -131,8 +131,8 @@ public class SampleCode {
     {
         // These are default transaction keys.
         // You can create your own keys in seconds by signing up for a sandbox account here: https://developer.authorize.net/sandbox/
-        String apiLoginId           = "mbld_api_%63ty4Rq";       
-        String transactionKey       = "123abc";
+		String apiLoginId           = "5KP3u95bQpv";
+        String transactionKey       = "346HZ32z3fP4hTG2";
         //Update the payedId with which you want to run the sample code
         String payerId 				= "6ZSCSYG33VP8Q";
         //Update the transactionId with which you want to run the sample code
